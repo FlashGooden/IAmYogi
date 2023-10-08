34cc662b54b7de3a6d347280f6c2b69b75277067
FlashGooden <Tro12185@gmail.com>
2023-10-07 23:26:14 -0400

    chore: add config files

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        green: {
+          950: "#1CAB68",
+        },
+        gray: {
+          950: "#202020",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
