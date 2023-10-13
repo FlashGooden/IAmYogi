b416bcdc15c610f2fccf9cccfc719c63e3b9f871
FlashGooden <Tro12185@gmail.com>
2023-10-12 21:02:34 -0400

    feat: update Services

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -2,38 +2,126 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Services.module.scss";
+import { domAnimation, LazyMotion } from "framer-motion";
+import { SlideInFromLeft } from "../../../utils/SlideInFromLeft";
+import HoverColor from "../../../utils/HoverColorCard";
+import Link from "next/link";
 
 const Services = () => {
   return (
-    <section id="services" className={"mt-20"}>
-      <div className={"grid justify-items-center"}>
-        <Image
-          src={"/Yoga-Photos/yoga-icon-pose-1.svg"}
-          alt={"Icon of woman meditating"}
-          height={"60"}
-          width={"60"}
-        />
-        <div className={`${styles.line}`}></div>
-        <div className="descripton grid justify-items-center">
-          <h2 className="services-heading text-center text-4xl text-green-950 font-semibold mt-10">
-            Your Spiritual Guide
-          </h2>
-          <p className={"text-black text-sm text-center mt-5 px-8"}>
-            Lorem ipsum proin gravida nibh vel velit auctor aliquet. Aenean
-            sollicitudin, lorem auci elit consequat ipsutissem niuis sed odio
-            sit amet nibh vulputate cursus a sit amet.
-          </p>
-        </div>
-        <div className={`${styles.servicesCards} mt-20`}>
-          <div className={`${styles.serviceCard} grid-area-1`}>test</div>
-          <div className={`${styles.serviceCard} grid-area-2`}>test</div>
-          <div className={`${styles.serviceCard} grid-area-3`}>test</div>
-          <div className={`${styles.serviceCard} grid-area-4`}>test</div>
-          <div className={`${styles.serviceCard} grid-area-5`}>test</div>
-          <div className={`${styles.serviceCard} grid-area-6`}>test</div>
-        </div>
-      </div>
-    </section>
+    <LazyMotion features={domAnimation}>
+      <SlideInFromLeft>
+        <section id="services" className={"mt-20"}>
+          <div className={"grid justify-items-center"}>
+            <Image
+              src={"/Yoga-Photos/yoga-icon-pose-1.svg"}
+              alt={"Icon of woman meditating"}
+              height={"60"}
+              width={"60"}
+            />
+            <div className={`${styles.line}`}></div>
+            <div className="descripton grid justify-items-center">
+              <h2 className="services-heading text-center text-4xl text-green-950 font-semibold mt-10">
+                Your Spiritual Guide
+              </h2>
+              <p className={"text-black text-sm text-center mt-5 px-8"}>
+                Lorem ipsum proin gravida nibh vel velit auctor aliquet. Aenean
+                sollicitudin, lorem auci elit consequat ipsutissem niuis sed
+                odio sit amet nibh vulputate cursus a sit amet.
+              </p>
+            </div>
+            <div className={`${styles.servicesCards} mt-20`}>
+              <div className={`${styles.serviceCard} inline grid-area-1`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-3.png"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">HATHA YOGA</Link>
+                  </div>
+                </HoverColor>
+              </div>
+              <div className={`${styles.serviceCard} grid-area-2`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-4.jpeg"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">VINYASA YOGA </Link>
+                  </div>
+                </HoverColor>
+              </div>
+              <div className={`${styles.serviceCard} grid-area-3`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-2.jpeg"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">INVERSIONS</Link>
+                  </div>
+                </HoverColor>
+              </div>
+              <div className={`${styles.serviceCard} grid-area-4`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-5.jpeg"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">
+                      CORPORATE/ ORGANIZATIONAL CLASSES
+                    </Link>
+                  </div>
+                </HoverColor>
+              </div>
+              <div className={`${styles.serviceCard} grid-area-5`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-6.jpeg"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">PRIVATE CLASSES</Link>
+                  </div>
+                </HoverColor>
+              </div>
+              <div className={`${styles.serviceCard} grid-area-6`}>
+                <HoverColor color={"#1CAB68"} className={`${styles.image}`}>
+                  <Image
+                    src={"/Yoga-Photos/services-4.jpeg"}
+                    alt={"Icon of woman meditating"}
+                    width={200}
+                    height={200}
+                    className={`${styles.image}`}
+                  />
+                  <div className={`${styles.cardText}`}>
+                    <Link href="/rental">PRIVATE YOGA TEAM</Link>
+                  </div>
+                </HoverColor>
+              </div>
+            </div>
+          </div>
+        </section>
+      </SlideInFromLeft>
+    </LazyMotion>
   );
 };
 
