547b6b10e7a03016c6009fc7c3c43c6fa90a1c3e
FlashGooden <Tro12185@gmail.com>
2023-10-18 00:23:45 -0400

    feat: reviews component + styling

---
+++ src/Components/Reviews/Reviews.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Reviews.module.scss";
+
+const reviews = [
+  {
+    quote:
+      "Aenean commodo ligula eget dolororem ipsum dolor sit amet, consectetuer adipiscing elit...",
+    author: "CYNTHIA FOWLER",
+    role: "Designer",
+  },
+  {
+    quote: "A Great Class for the Day, consectetuer adipiscing elit...",
+    author: "FELLA CUTIE",
+    role: "Designer",
+  },
+];
+
+const ReviewSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(goToNextReview, 6000); // Switch every 3 seconds
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [currentIndex]); // We re-run the effect every time currentIndex changes
+  const goToNextReview = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  };
+
+  const review = reviews[currentIndex];
+
+  return (
+    <div className={`${styles.reviewSliderContainer} mt-20`}>
+      <div className={`${styles.leftFlower}`}></div>
+      <div className={`${styles.reviewContent}`}>
+        <h2 className={`${styles.heading} mt-40`}>HAPPY CUSTOMERS</h2>
+        <p className={`${styles.quote}`}>{review.quote}</p>
+        <h3 className={`${styles.author}`}>{review.author}</h3>
+        <h4 className={`${styles.role}`}>{review.role}</h4>
+        <button className={`${styles.button}`} onClick={goToNextReview}>
+          Next
+        </button>
+      </div>
+      <div className={`${styles.rightFlower}`}></div>
+    </div>
+  );
+};
+
+export default ReviewSlider;
