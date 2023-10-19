14d251e6d081500ae1557d348b1dc7645e19513e
FlashGooden <Tro12185@gmail.com>
2023-10-19 00:45:38 -0400

    feat: reviews update logic + styling

--- src/Components/Reviews/Reviews.tsx
+++ src/Components/Reviews/Reviews.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Reviews.module.scss";
+import Image from "next/image";
 
 const reviews = [
   {
@@ -31,13 +32,20 @@ const ReviewSlider = () => {
   const review = reviews[currentIndex];
 
   return (
-    <div className={`${styles.reviewSliderContainer} mt-20`}>
+    <div className={`${styles.reviewSliderContainer} text-white mt-20`}>
       <div className={`${styles.leftFlower}`}></div>
+      <Image
+        src={"/Yoga-Photos/testimonials-quote.png"}
+        alt={"picture of quote"}
+        height={"80"}
+        width={"80"}
+        className={`${styles.quotePNG}`}
+      />
       <div className={`${styles.reviewContent}`}>
         <h2 className={`${styles.heading} mt-40`}>HAPPY CUSTOMERS</h2>
         <p className={`${styles.quote}`}>{review.quote}</p>
         <h3 className={`${styles.author}`}>{review.author}</h3>
-        <h4 className={`${styles.role}`}>{review.role}</h4>
+        <h4 className={`${styles.role} text-sm`}>{review.role}</h4>
         <button className={`${styles.button}`} onClick={goToNextReview}>
           Next
         </button>
